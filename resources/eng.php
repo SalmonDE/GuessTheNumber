6f719fea7dc9466681dc892dc663812b42715af0
Aericio <Aericio@users.noreply.github.com>
2016-07-05 00:05:09 -1000

    Temp. Google Translated Config D:

--- resources/eng.php
+++ resources/eng.php
@@ -1,4 +1,49 @@
-<?php
-#$StartMessage = "has been enabled!";
-#$DisableMessage = "has been disabled!";
+<? Php
+# Configurable Messages
+#
+# / Guess game abort News
+$ Gameaborted = 'This quiz has been canceled!'; // If each player displayed
+$ Nogameactive = 'There is currently no active Quiz!'; // Displayed command user
+# / Guess game solution News
+$ Normal solution = 'Seeking number:'; // After this message, the solution is displayed
+$ Square solution = 'The requested square number:'; // After this message, the solution is displayed
+# Other News
+$ Nopermission = 'You must not!';
+$ Gamealreadyactive = 'A Quiz is already active!';
+# Normal guessing game
+$ Header = '* ------- Number Quiz ------- *';
+$ Firstline = 'Writing in the chat a';
+$ Second line = "number zwischen§d {min} §bund§d {max}"; // {min} and {max} are the limits of the game
+$ Third line = 'if it's the number who sought';
+$ Fourthline = 'is, you win something! : D ';
+$ Bottom = '* ------- Number Quiz ------- *';
+# Error messages
+$ Numtoohigh = "This number is too high, the quiz uses the numbers zwischen§d {min} §cund§d {max}!";
+$ Notright = 'Unfortunately, this is not the required number! ; ( ';
+#
+# Winner News
+$ Congratulation = "Congratulations, {name}". '!';
+$ Right number = "The required number was: Once upon {number}";. '.'
+$ Message = winner "You have {count} times {won itemname}!";
+# {Count}: Number of price (Item)
+# {Itemname}: Name of the price
+#
+# Squares guessing game
+$ Qheader = '* --- perfect square Quiz --- *';
+$ Qfirstline = 'Writing in the chat, the';
+$ Qsecondline = "square number von§d {qnum}". '.';
+$ Qthirdline = 'if it's the number who sought';
+$ Qfourthline = 'is, you win something! : D ';
+$ Qbottom = '* --- perfect square Quiz --- *';
+# Error messages
+$ Qnotright = 'Unfortunately, this is not the desired square number! ; ( ';
+# Winner News
+$ Qcongratulation = 'Congratulations, {name}!';
+$ Qrightnumber = "The perfect square von§9 {qnum} {§6ist§b numq}"; // {qnum} is the output number and {numq} is the square number
+$ Qwinnermessage = "You won {count} times {itemname}!";
+# {Count}: Number of price (Item)
+# {Itemname}: Name of the price
+#
+# Help message for both types of rate Game
+$ Advice = 'To participate, your chat message must consist only of figures!';
 ?>
