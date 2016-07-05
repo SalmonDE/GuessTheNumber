c731f8b0a36fd38cd46afa4e48e4b5c075d59aa8
Aericio <Aericio@users.noreply.github.com>
2016-07-05 00:34:53 -1000

    Hopefully it is good enough, for a person to understand.

--- resources/eng.php
+++ resources/eng.php
@@ -2,8 +2,8 @@
 # Configurable Messages: English
 
 # Guess Game Abort Messages
-$gameaborted = 'Quiz has been aborted!!'; // If each player displayed
-$nogameactive = 'There are no active games availible!'; // Displayed command user
+$gameaborted = 'Quiz has been aborted!!';
+$nogameactive = 'There are no active games availible!';
 
 # Guess Game Solution Messages
 $normalsolution = 'Normal Game Solution:'; // Solution is displayed for Normal Number, after this message.
@@ -16,18 +16,18 @@ $gamealreadyactive = 'A quiz has already been activated!';
 # Normal Guessing Game
 $header = '------- Number Quiz -------';
 $firstline = 'First, write a number in chat';
-$secondline = "between§d {min} §band§d {max}."; // {min} and {max} are the limits of the game
+$secondline = "between §d{min} §band §d{max}."; // {min} and {max} are the limits of the game
 $thirdline = 'If the guesser is correct,';
-$fourthline = 'the guesser will win something!';
+$fourthline = 'the guesser will be rewarded!';
 $bottom = '------- Number Quiz -------';
 
 # Error Messages
-$numtoohigh = "This number is too high, the quiz uses the numbers zwischen§d {min} §cund§d {max}!";
+$numtoohigh = "You can only use numbers between §d{min} §cand §d{max}!";
 $notright = 'Unfortunately, that is not the correct answer!';
 
 # Winner Messages
 $congratulation = "Congratulations, {name}". '!';
-$rightnumber = "The required number was: {number}";
+$rightnumber = "The correct number was: {number}";
 $message = "You have been rewarded {count} of {itemname}!";
 # {count}: Amount of the item they are given.
 # {itemname}: Name of the Prize! (ex: dirt)
@@ -37,10 +37,10 @@ $qheader = '--- Square Root Quiz ---';
 $qfirstline = 'First, write a number in chat';
 $qsecondline = "the square root of §d{qnum}";
 $qthirdline = 'If the guesser is correct,';
-$qfourthline = 'the guesser will win something!';
+$qfourthline = 'the guesser will be rewarded!';
 $qbottom = '--- Square Root Quiz ---';
 
-# Error Message for Square
+# Error Message for Squares
 $qnotright = 'Unfortunately, that is not the correct square number!';
 
 # Winner Message
