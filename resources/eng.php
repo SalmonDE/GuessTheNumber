28a85e14de0ffe4501e77690a77bcd8e7939ce0f
Aericio <Aericio@users.noreply.github.com>
2016-07-05 00:13:09 -1000

    Not done yet, but its enough for now?

--- resources/eng.php
+++ resources/eng.php
@@ -1,49 +1,55 @@
-<? Php
-# Configurable Messages
-#
-# / Guess game abort News
-$ Gameaborted = 'This quiz has been canceled!'; // If each player displayed
-$ Nogameactive = 'There is currently no active Quiz!'; // Displayed command user
-# / Guess game solution News
-$ Normal solution = 'Seeking number:'; // After this message, the solution is displayed
-$ Square solution = 'The requested square number:'; // After this message, the solution is displayed
-# Other News
-$ Nopermission = 'You must not!';
-$ Gamealreadyactive = 'A Quiz is already active!';
-# Normal guessing game
-$ Header = '* ------- Number Quiz ------- *';
+<?php
+# Configurable Messages: English
+
+# Guess Game Abort Messages
+$ Gameaborted = 'Quiz has been aborted!!'; // If each player displayed
+$ Nogameactive = 'There are no active games availible!'; // Displayed command user
+
+# Guess Game Solution Messages
+$ Normal solution = 'Answer for Normal Game Solution:'; // Solution is displayed for Normal Number, after this message.
+$ Square solution = 'Answer for Square Game Solution:'; // Solution is displayed for Square Number, after this message.
+
+# Other Messages
+$ Nopermission = 'You do not have permission to run this command!';
+$ Gamealreadyactive = 'A quiz has already been activated!';
+
+# Normal Guessing Game
+$ Header = '------- Number Quiz -------';
 $ Firstline = 'Writing in the chat a';
 $ Second line = "number zwischen§d {min} §bund§d {max}"; // {min} and {max} are the limits of the game
 $ Third line = 'if it's the number who sought';
-$ Fourthline = 'is, you win something! : D ';
-$ Bottom = '* ------- Number Quiz ------- *';
-# Error messages
+$ Fourthline = 'is, you win something!';
+$ Bottom = '------- Number Quiz -------';
+
+# Error messages -- what is zwischen xd
 $ Numtoohigh = "This number is too high, the quiz uses the numbers zwischen§d {min} §cund§d {max}!";
-$ Notright = 'Unfortunately, this is not the required number! ; ( ';
-#
+$ Notright = 'Unfortunately, that is not the correct answer!';
+
 # Winner News
 $ Congratulation = "Congratulations, {name}". '!';
 $ Right number = "The required number was: Once upon {number}";. '.'
 $ Message = winner "You have {count} times {won itemname}!";
 # {Count}: Number of price (Item)
 # {Itemname}: Name of the price
-#
-# Squares guessing game
-$ Qheader = '* --- perfect square Quiz --- *';
+
+# Squares Guessing Game
+$ Qheader = '--- Perfect Square Quiz ---';
 $ Qfirstline = 'Writing in the chat, the';
 $ Qsecondline = "square number von§d {qnum}". '.';
 $ Qthirdline = 'if it's the number who sought';
-$ Qfourthline = 'is, you win something! : D ';
-$ Qbottom = '* --- perfect square Quiz --- *';
-# Error messages
-$ Qnotright = 'Unfortunately, this is not the desired square number! ; ( ';
-# Winner News
+$ Qfourthline = 'is, you win something!';
+$ Qbottom = '--- Perfect Sqaure Quiz ---';
+
+# Error Message
+$ Qnotright = 'Unfortunately, that is not the correct square number!';
+
+# Winner Message
 $ Qcongratulation = 'Congratulations, {name}!';
-$ Qrightnumber = "The perfect square von§9 {qnum} {§6ist§b numq}"; // {qnum} is the output number and {numq} is the square number
+$ Qrightnumber = "The perfect square von§9 {qnum} {§6is§b numq}"; // {qnum} is the output number and {numq} is the square number
 $ Qwinnermessage = "You won {count} times {itemname}!";
 # {Count}: Number of price (Item)
 # {Itemname}: Name of the price
-#
+
 # Help message for both types of rate Game
 $ Advice = 'To participate, your chat message must consist only of figures!';
 ?>
