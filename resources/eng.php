41d00c6324466d82b398a0d2526f07d6238b2b0b
Aericio <Aericio@users.noreply.github.com>
2016-07-05 00:18:51 -1000

    i h8 google D:

--- resources/eng.php
+++ resources/eng.php
@@ -2,54 +2,54 @@
 # Configurable Messages: English
 
 # Guess Game Abort Messages
-$ Gameaborted = 'Quiz has been aborted!!'; // If each player displayed
-$ Nogameactive = 'There are no active games availible!'; // Displayed command user
+$gameaborted = 'Quiz has been aborted!!'; // If each player displayed
+$nogameactive = 'There are no active games availible!'; // Displayed command user
 
 # Guess Game Solution Messages
-$ Normal solution = 'Answer for Normal Game Solution:'; // Solution is displayed for Normal Number, after this message.
-$ Square solution = 'Answer for Square Game Solution:'; // Solution is displayed for Square Number, after this message.
+$normalsolution = 'Answer for Normal Game Solution:'; // Solution is displayed for Normal Number, after this message.
+$squaresolution = 'Answer for Square Game Solution:'; // Solution is displayed for Square Number, after this message.
 
 # Other Messages
-$ Nopermission = 'You do not have permission to run this command!';
-$ Gamealreadyactive = 'A quiz has already been activated!';
+$nopermission = 'You do not have permission to run this command!';
+$gamealreadyactive = 'A quiz has already been activated!';
 
 # Normal Guessing Game
-$ Header = '------- Number Quiz -------';
-$ Firstline = 'Writing in the chat a';
-$ Second line = "number zwischen§d {min} §bund§d {max}"; // {min} and {max} are the limits of the game
-$ Third line = 'if it's the number who sought';
-$ Fourthline = 'is, you win something!';
-$ Bottom = '------- Number Quiz -------';
+$header = '------- Number Quiz -------';
+$firstline = 'Writing in the chat a';
+$secondline = "number zwischen§d {min} §bund§d {max}"; // {min} and {max} are the limits of the game
+$thirdline = 'if its the number who sought';
+$fourthline = 'is, you win something!';
+$bottom = '------- Number Quiz -------';
 
 # Error messages -- what is zwischen xd
-$ Numtoohigh = "This number is too high, the quiz uses the numbers zwischen§d {min} §cund§d {max}!";
-$ Notright = 'Unfortunately, that is not the correct answer!';
+$numtoohigh = "This number is too high, the quiz uses the numbers zwischen§d {min} §cund§d {max}!";
+$notright = 'Unfortunately, that is not the correct answer!';
 
 # Winner News
-$ Congratulation = "Congratulations, {name}". '!';
-$ Right number = "The required number was: Once upon {number}";. '.'
-$ Message = winner "You have {count} times {won itemname}!";
+$congratulation = "Congratulations, {name}". '!';
+$rightnumber = "The required number was: Once upon {number}";. '.'
+$message = winner "You have {count} times {won itemname}!";
 # {Count}: Number of price (Item)
 # {Itemname}: Name of the price
 
 # Squares Guessing Game
-$ Qheader = '--- Perfect Square Quiz ---';
-$ Qfirstline = 'Writing in the chat, the';
-$ Qsecondline = "square number von§d {qnum}". '.';
-$ Qthirdline = 'if it's the number who sought';
-$ Qfourthline = 'is, you win something!';
-$ Qbottom = '--- Perfect Sqaure Quiz ---';
+$qheader = '--- Perfect Square Quiz ---';
+$qfirstline = 'Writing in the chat, the';
+$qsecondline = "square number von§d {qnum}". '.';
+$qthirdline = 'if its the number who sought';
+$qfourthline = 'is, you win something!';
+$qbottom = '--- Perfect Sqaure Quiz ---';
 
 # Error Message
-$ Qnotright = 'Unfortunately, that is not the correct square number!';
+$qnotright = 'Unfortunately, that is not the correct square number!';
 
 # Winner Message
-$ Qcongratulation = 'Congratulations, {name}!';
-$ Qrightnumber = "The perfect square von§9 {qnum} §6is§b {numq}"; // {qnum} is the output number and {numq} is the square number
-$ Qwinnermessage = "You won {count} times {itemname}!";
+$qcongratulation = 'Congratulations, {name}!';
+$qrightnumber = "The perfect square von§9 {qnum} §6is§b {numq}"; // {qnum} is the output number and {numq} is the square number
+$qwinnermessage = "You won {count} times {itemname}!";
 # {Count}: Number of price (Item)
 # {Itemname}: Name of the price
 
 # Help message for both types of rate Game
-$ Advice = 'To participate, your chat message must consist only of figures!';
+$advice = 'To participate, your chat message must consist only of figures!';
 ?>
