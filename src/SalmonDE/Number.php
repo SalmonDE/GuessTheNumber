54484d62f67d4c27b91c9f62249099fe8ce411e5
SalmonDE <salmonde@salmonde.de>
2016-10-09 19:44:04 +0200

    Look into the description ->
    - Only using /guessgame by now (/guessgame normal)
    - Added plus guessgame (+) idea by adalin3 aka Anastasia
    - Added minus guessgame (-)
    - Removed not used/necessary things

--- src/SalmonDE/Number.php
+++ src/SalmonDE/Number.php
@@ -40,53 +40,83 @@ class Number extends PluginBase implements Listener
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		  if(strtolower($cmd->getName()) == 'guessgamesolution'){
-          if($sender->hasPermission('guessthenumber.solution')){
-				      if(isset($this->information)){
-					        $sender->sendMessage(TF::BLUE.str_ireplace('{value}', $this->information['solution'], $this->getMessages()['solution']));
-				      }else{
-					        $sender->sendMessage(TF::RED.$this->getMessages()['nogameactive']);
-				      }
-			      }
-		  }elseif(strtolower($cmd->getName()) == 'guessgameabort'){
-				  if($sender->hasPermission('guessthenumber.abort')){
-					    if(isset($this->information)){
-					        unset($this->information);
-				          $this->getServer()->broadcastMessage(TF::RED.TF::BOLD.$this->getMessages()['gameaborted']);
-						      return true;
-					    }else{
-						      $sender->sendMessage(TF::GOLD.$this->getMessages()['nogameactive']);
-						      return true;
-					    }
-				    }else{
-					    $sender->sendMessage(TF::GOLD.$this->getMessages()['nogameactive']);
-					    return true;
-				    }
-		  }elseif(isset($this->information)){
-			    $sender->sendMessage(TF::RED.$this->getMessages()['gamealreadyactive']);
-		  }else{
-		      if(strtolower($cmd->getName()) == 'guessgame'){
-              $this->information = ['behavior' => 1, 'solution' => mt_rand($this->min, $this->max)];
-				      $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['header']);
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['firstline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['secondline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['thirdline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['fourthline']));
-				      $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['bottom']);
-				      $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
-		          return true;
-		      }elseif(strtolower($cmd->getName()) == 'guessgamesquare'){
-		          $num = mt_rand(1, 20);
-              $this->information = ['behavior' => 2, 'solution' => $num * $num];
-				      $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['header']);
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['firstline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['secondline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['thirdline']));
-				      $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['fourthline']));
-				      $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['bottom']);
-				      $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
-					    return true;
+		  if(count($args) <> 1){
+		      if(strtolower($args[0]) == 'solution'){
+              if($sender->hasPermission('guessthenumber.solution')){
+				          if(isset($this->information)){
+					            $sender->sendMessage(TF::BLUE.str_ireplace('{value}', $this->information['solution'], $this->getMessages()['solution']));
+				          }else{
+					            $sender->sendMessage(TF::RED.$this->getMessages()['nogameactive']);
+				          }
+			        }
+		      }elseif(strtolower($args[0]) == 'abort'){
+				      if($sender->hasPermission('guessthenumber.abort')){
+					        if(isset($this->information)){
+					            unset($this->information);
+				              $this->getServer()->broadcastMessage(TF::RED.TF::BOLD.$this->getMessages()['gameaborted']);
+						          return true;
+					        }else{
+						          $sender->sendMessage(TF::GOLD.$this->getMessages()['nogameactive']);
+						          return true;
+					        }
+				        }else{
+					          $sender->sendMessage(TF::GOLD.$this->getMessages()['nogameactive']);
+					          return true;
+				        }
+		      }elseif(isset($this->information)){
+			        $sender->sendMessage(TF::RED.$this->getMessages()['gamealreadyactive']);
+		      }else{
+		          if(strtolower($args[0]) == 'normal'){
+                  $this->information = ['behavior' => 1, 'solution' => mt_rand($this->min, $this->max)];
+				          $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['header']);
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['firstline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['secondline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['thirdline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['fourthline']));
+				          $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['bottom']);
+				          $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
+		              return true;
+		          }elseif(strtolower($args[0]) == 'square'){
+		              $num = mt_rand(1, 20);
+                  $this->information = ['behavior' => 2, 'solution' => $num * $num];
+				          $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['header']);
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace('{value}', $num, $this->getMessages()['Square']['firstline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace('{value}', $num, $this->getMessages()['Square']['secondline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace('{value}', $num, $this->getMessages()['Square']['thirdline']));
+				          $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace('{value}', $num, $this->getMessages()['Square']['fourthline']));
+				          $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['bottom']);
+				          $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
+					        return true;
+		          }elseif(strtolower($args[0]) == 'plus'){
+								  $num = mt_rand(1, 1000);
+									$num2 = mt_rand(1, 1000);
+								  $this->information = ['behavior' => 3, 'solution' => $num + $num2, 'num1' => $num, 'num2' => $num2]; # Idea by adalin3 (Anastasia)
+								  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Plus']['header']);
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Plus']['firstline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Plus']['secondline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Plus']['thirdline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Plus']['fourthline']));
+								  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Plus']['bottom']);
+								  $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
+								  return true;
+							}elseif(strtolower($args[0]) == 'minus'){
+								  $num = mt_rand(1, 2000);
+								  $num2 = mt_rand(1, 1000);
+								  $this->information = ['behavior' => 4, 'solution' => $num - $num2, 'num1' => $num, 'num2' => $num2];
+								  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Minus']['header']);
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Minus']['firstline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Minus']['secondline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Minus']['thirdline']));
+								  $this->getServer()->broadcastMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$num, $num2], $this->getMessages()['Minus']['fourthline']));
+								  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Minus']['bottom']);
+								  $this->getServer()->broadcastMessage(TF::RED.$this->getMessages()['advice']);
+								  return true;
+							}else{
+								  return false;
+							}
 		      }
+		  }else{
+			    return false;
 		  }
 	}
 
@@ -110,26 +140,38 @@ class Number extends PluginBase implements Listener
 		  if(isset($this->information)){
 			    $player = $event->getPlayer();
 			    if($this->information['behavior'] == 1){
-				      $player->sendMessage("\n");
 			 	      $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['header']);
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['firstline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['secondline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['thirdline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace(['{min}', '{max}'], [$this->min, $this->max], $this->getMessages()['fourthline']));
 				      $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['bottom']);
-              $player->sendMessage("\n");
 				      $player->sendMessage(TF::RED.$this->getMessages()['advice']);
 			    }elseif($this->information['behavior'] == 2){
-				      $player->sendMessage(TF::GOLD.TF::BOLD."\n");
 				      $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['header']);
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['firstline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['secondline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['thirdline']));
 				      $player->sendMessage(TF::AQUA.TF::BOLD.str_ireplace('{value}', sqrt($this->information['solution']), $this->getMessages()['Square']['fourthline']));
 				      $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Square']['bottom']);
-              $player->sendMessage("\n");
 				      $player->sendMessage(TF::RED.$this->getMessages()['advice']);
-			    }
+			    }elseif($this->information['behavior'] == 3){
+						  $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Plus']['header']);
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Plus']['firstline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Plus']['secondline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Plus']['thirdline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Plus']['fourthline']));
+						  $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Plus']['bottom']);
+						  $player->sendMessage(TF::RED.$this->getMessages()['advice']);
+					}elseif($this->information['behavior'] == 4){
+						  $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Minus']['header']);
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Minus']['firstline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Minus']['secondline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Minus']['thirdline']));
+						  $player->sendMessage(TF::AQUA.TF::BOLD.str_replace(['{value}', '{value2}'], [$this->information['num1'], $this->information['num2']], $this->getMessages()['Minus']['fourthline']));
+						  $player->sendMessage(TF::GOLD.TF::BOLD.$this->getMessages()['Minus']['bottom']);
+						  $player->sendMessage(TF::RED.$this->getMessages()['advice']);
+					}
 		  }
 	}
 
@@ -139,29 +181,36 @@ class Number extends PluginBase implements Listener
 			}
 			unset($this->queue);
 		  $name = $winner->getDisplayName();
+			foreach($this->getServer()->getOnlinePlayers() as $player){
+					$player->getLevel()->addSound(new FizzSound($player->getPosition()));
+			}
+			$this->getServer()->broadcastMessage(TF::GREEN.TF::BOLD.str_replace('{value}', $name, $this->getMessages()['congratulation']));
 		  if($this->information['behavior'] == 1){
-			    foreach($this->getServer()->getOnlinePlayers() as $player){
-				      $player->getLevel()->addSound(new FizzSound($player->getPosition()));
-			    }
-			    $this->getServer()->broadcastMessage(TF::GREEN.TF::BOLD.str_ireplace('{value}', $name, $this->getMessages()['congratulation']));
-			    $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_ireplace('{value}', $this->information['solution'], $this->getMessages()['rightnumber']));
+			    $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_replace('{value}', $this->information['solution'], $this->getMessages()['rightnumber']));
 					unset($this->information);
 			    $item = explode(':', $this->getConfig()->get('Item'));
 			    $itemname = Item::get($item[0])->getName();
-			    $winner->getInventory()->addItem(new Item($item[0], $item[1], $item[2]));
-		      $winner->sendMessage(TF::GREEN.TF::BOLD.str_ireplace(['{count}', '{itemname}'], [$item[2], $itemname], $this->getMessages()['winnermessage']));
 		  }elseif($this->information['behavior'] == 2){
-			    foreach($this->getServer()->getOnlinePlayers() as $player){
-				      $player->getLevel()->addSound(new FizzSound($player->getPosition()));
-			    }
-			    $this->getServer()->broadcastMessage(TF::GREEN.TF::BOLD.str_ireplace('{value}', $name, $this->getMessages()['congratulation']));
-			    $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_ireplace(['{num}', '{solution}'], [sqrt($this->information['solution']), $this->information['solution']], $this->getMessages()['Square']['rightnumber']));
+			    $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_replace(['{num}', '{solution}'], [sqrt($this->information['solution']), $this->information['solution']], $this->getMessages()['Square']['rightnumber']));
 					unset($this->information);
 			    $item = explode(':', $this->getConfig()->get('SquareItem'));
 			    $itemname = Item::get($item[0])->getName();
-			    $winner->getInventory()->addItem(new Item($item[0], $item[1], $item[2]));
-		      $winner->sendMessage(TF::LIGHT_PURPLE.TF::BOLD.str_ireplace(['{count}', '{itemname}'], [$item[2], $itemname], $this->getMessages()['winnermessage']));
-      }
+      }elseif($this->information['behavior'] == 3){
+				  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_replace(['{num}', '{num2}', '{solution}'], [$this->information['num1'], $this->information['num2'], $this->information['solution']], $this->getMessages()['Plus']['rightnumber']));
+				  unset($this->information);
+				  $item = explode(':', $this->getConfig()->get('PlusItem'));
+				  $itemname = Item::get($item[0])->getName();
+			}elseif($this->information['behavior'] == 4){
+				  $this->getServer()->broadcastMessage(TF::GOLD.TF::BOLD.str_replace(['{num}', '{num2}', '{solution}'], [$this->information['num1'], $this->information['num2'], $this->information['solution']], $this->getMessages()['Minus']['rightnumber']));
+			  	unset($this->information);
+				  $item = explode(':', $this->getConfig()->get('MinusItem'));
+				  $itemname = Item::get($item[0])->getName();
+			}else{
+	 		   $item = explode(':', '0:0:0');
+	 		   $itemname = Item::get($item[0])->getName();
+			}
+			$winner->getInventory()->addItem(new Item($item[0], $item[1], $item[2]));
+			$winner->sendMessage(TF::GREEN.TF::BOLD.str_replace(['{count}', '{itemname}'], [$item[2], $itemname], $this->getMessages()['winnermessage']));
 	}
 
 	public function update(){
